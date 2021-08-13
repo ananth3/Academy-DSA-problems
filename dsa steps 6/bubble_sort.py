bea6db56ad0c4e8e68b4500f13f6df02842fb170
ananth3 <35170699+ananth3@users.noreply.github.com>
2021-08-13 23:48:52 +0530

    Add files via upload

---
+++ dsa steps 6/bubble_sort.py
@@ -0,0 +1,10 @@
+def bubbleSort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+arr = [6,7,4,1,3,9]
+bubbleSort(arr)
+print(arr)
---
+++ dsa steps 6/selection_sort.py
@@ -0,0 +1,11 @@
+def selectionSort(arr):
+    for i in range(len(arr)):
+        min_idx = i
+        for j in range(i+1, len(arr)):
+            if arr[min_idx] > arr[j]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+arr = [6,7,4,1,3,9]
+selectionSort(arr)
+print(arr)
